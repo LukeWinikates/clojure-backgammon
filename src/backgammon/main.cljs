de8ecf2c9f43078a652d2650f17348752e77d81e
Luke Winikates <luke.winikates@gmail.com>
2014-04-05 10:13:36 -0700

    Added board-view component

--- src/backgammon/main.cljs
+++ src/backgammon/main.cljs
@@ -2,10 +2,62 @@
  (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
-(def app-state (atom {:text "Hello world" }))
+(defn board []
+  {:pips (vec (repeatedly 24 pip))})
+
+(defn nack-board []
+  {:pips [(pip :white 2)
+          (pip :white 2)
+          (pip)
+          (pip)
+          (pip)
+          (pip :black 4)
+          (pip)
+          (pip :black 3)
+          (pip)
+          (pip)
+          (pip)
+          (pip :white 4)
+          (pip :black 4)
+          (pip)
+          (pip)
+          (pip)
+          (pip :white 3)
+          (pip)
+          (pip :white 4)
+          (pip)
+          (pip)
+          (pip)
+          (pip :black 2)
+          (pip :black 2)]})
+
+(defn pip
+  ([] { :owner nil :count 0 })
+  ([owner size] { :owner owner :count size } ))
+
+(def app-state (atom {:board (nack-board) }))
+
+(defn render-pip [pip]
+  (let [count (:count pip)]
+  (dom/li nil
+    (if (= count 0)
+      "empty"
+      (str (name (:owner pip)) ": " count)))))
+
+(defn board-view [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div #js{:className "board"}
+        (dom/h2 nil "Board")
+        (dom/div nil "Black")
+        (apply dom/ul nil
+          (map render-pip (:pips (:board app))))
+        (dom/div nil "White")))))
 
 (om/root
-  (fn [app owner]
-    (dom/h2 nil (:text app)))
+  board-view
   app-state
   {:target (. js/document (getElementById "root"))})
+
+
