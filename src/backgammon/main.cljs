edcd1779d887bc6701f302092be574a200633499
Luke Winikates <luke.winikates@gmail.com>
2014-04-05 14:53:39 -0700

    Added indexes to pips; stopped tracking main.js

--- src/backgammon/main.cljs
+++ src/backgammon/main.cljs
@@ -3,48 +3,49 @@
             [om.dom :as dom :include-macros true]
             [cljs.core.async :refer [put! chan <!]]))
 (defn pip
-  ([] { :owner nil :count 0 })
-  ([owner size] { :owner owner :count size } ))
+  ([idx] { :index idx :owner nil :count 0 })
+  ([idx owner size] { :index idx :owner owner :count size } ))
 
-(defn board []
-  {:pips (vec (repeatedly 24 pip))})
+;(defn board []
+  ;{:pips (vec (repeatedly 24 pip))})
 
 (defn nack-board []
-  {:pips [(pip :white 2)
-          (pip :white 2)
-          (pip)
-          (pip)
-          (pip)
-          (pip :black 4)
-          (pip)
-          (pip :black 3)
-          (pip)
-          (pip)
-          (pip)
-          (pip :white 4)
-          (pip :black 4)
-          (pip)
-          (pip)
-          (pip)
-          (pip :white 3)
-          (pip)
-          (pip :white 4)
-          (pip)
-          (pip)
-          (pip)
-          (pip :black 2)
-          (pip :black 2)]})
+  {:pips [(pip 1 :white 2)
+          (pip 2 :white 2)
+          (pip 3)
+          (pip 4)
+          (pip 5)
+          (pip 6 :black 4)
+          (pip 7)
+          (pip 8 :black 3)
+          (pip 9)
+          (pip 10)
+          (pip 11)
+          (pip 12 :white 4)
+          (pip 13 :black 4)
+          (pip 14)
+          (pip 15)
+          (pip 16)
+          (pip 17 :white 3)
+          (pip 18)
+          (pip 19 :white 4)
+          (pip 20)
+          (pip 21)
+          (pip 22)
+          (pip 23 :black 2)
+          (pip 24 :black 2)]})
 
 (def app-state (atom {:board (nack-board) }))
 
-
+(defn send-move [pip]
+  (.log js/console "clicked!" (:index @pip)))
 
 (defn pip-view [pip owner]
   (reify
     om/IRenderState
     (render-state [this owner]
       (let [count (:count pip)]
-      (dom/li #js { :onClick (fn [e] (.log js/console "clicked!") )}
+      (dom/li #js { :onClick (fn [e] (send-move pip) )}
         (if (= count 0)
           "empty"
           (str (name (:owner pip)) ": " count)))))))
